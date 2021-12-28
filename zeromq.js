c8259c995e5704d58ccea3331cf3964353cfad9d
Richard Cornelissen <ricorn@gmail.com>
2021-12-28 21:05:25 +0100

    format js file

--- zeromq.js
+++ zeromq.js
@@ -8,31 +8,31 @@ sock.connect("tcp://pubsub.besteffort.ndovloket.nl:7658");
 sock.subscribe("/ARR/");
 console.log("Subscriber connected to port 7658");
 
-sock.on("message", function (topic, message) {
-  console.log(
-    "received a message related to:",
-    topic.toString(),
-    "containing message:",
-    parseString(zlib.gunzipSync(message).toString(), function (err, result) {
-	if (result["VV_TM_PUSH"]["KV6posinfo"][0].INIT != undefined) {
-		for (x = 0; x < result["VV_TM_PUSH"]["KV6posinfo"][0].INIT.length; x++) {
-		console.log(
-        	result["VV_TM_PUSH"]["KV6posinfo"][0].INIT[x]
-		)
-		fs.appendFile('arr.txt', "INIT: " + JSON.stringify(result["VV_TM_PUSH"]["KV6posinfo"][0].INIT[x]) + "\n",
-			function(err) {
-				if (err) throw err;
-				console.log('Saved!');
-				});
-		for (x = 0; x < result["VV_TM_PUSH"]["KV6posinfo"][0].END.length; x++) {
-			fs.appendFile('arr.txt', "END: " + JSON.stringify(result["VV_TM_PUSH"]["KV6posinfo"][0].END[x]) + "\n",
-				function(err) {
-					if (err) throw err;
-					console.log('Saved!');
-					});
-		}
-			};
-					}
-				})
-		)}
-);
+sock.on("message", function(topic, message) {
+    console.log(
+        "received a message related to:",
+        topic.toString(),
+        "containing message:",
+        parseString(zlib.gunzipSync(message).toString(), function(err, result) {
+            if (result["VV_TM_PUSH"]["KV6posinfo"][0].INIT != undefined) {
+                for (x = 0; x < result["VV_TM_PUSH"]["KV6posinfo"][0].INIT.length; x++) {
+                    console.log(
+                        result["VV_TM_PUSH"]["KV6posinfo"][0].INIT[x]
+                    )
+                    fs.appendFile('arr.txt', "INIT: " + JSON.stringify(result["VV_TM_PUSH"]["KV6posinfo"][0].INIT[x]) + "\n",
+                        function(err) {
+                            if (err) throw err;
+                            console.log('Saved!');
+                        });
+                    for (x = 0; x < result["VV_TM_PUSH"]["KV6posinfo"][0].END.length; x++) {
+                        fs.appendFile('arr.txt', "END: " + JSON.stringify(result["VV_TM_PUSH"]["KV6posinfo"][0].END[x]) + "\n",
+                            function(err) {
+                                if (err) throw err;
+                                console.log('Saved!');
+                            });
+                    }
+                };
+            }
+        })
+    )
+});
